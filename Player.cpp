42431a1e6e593cac86f35b78089bb3acb5e26dbe
陽悦 佐藤 <youetsux@gmail.com>
2024-05-09 14:25:58 +0900

    ちょい解説追加

--- Player.cpp
+++ Player.cpp
@@ -44,35 +44,34 @@ void Player::GetInputData()
 void Player::Update()
 {
 	cdtimer_->Update();//カウントダウン
+	//Debug::Log(cdtimer_->IsTimerRun(), true);
 
-	float lerpRate = 1.0f - cdtimer_->GetTime()/ DTIME;
-	Debug::Log(cdtimer_->IsTimerRun(), true);
-	//次の時間Lerp実装
 	GetInputData();
 	if (moveDir_ == MNONE)
 		return;
+
 	if (cdtimer_->IsTimerRun())
 	{
+		float lerpRate = 1.0f - cdtimer_->GetTime() / DTIME;
 		XMVECTOR pos = XMVectorLerp(V[0], V[1], lerpRate);
 		XMStoreFloat3(&transform_.position_, pos);
-		float rotAngle = 90.0 * sin(lerpRate * XM_PI);
-		
-		if (XMVectorGetX(V[1]) < XMVectorGetX(V[0]))
-		{
-			rotAngle = -rotAngle;
-		}
-		transform_.rotate_.y = rotAngle;
+		//ここのコメントを外すと回転角に
+		// 補間レートがが 0～0.5～1の時に
+		// Sin関数を使って0～ 90～0度の曲線補間をする
+		//float rotAngle = 90.0 * sin(lerpRate * XM_PI);
+		//
+		//if (XMVectorGetX(V[1]) < XMVectorGetX(V[0]))
+		//{
+		//	rotAngle = -rotAngle;
+		//}
+		//transform_.rotate_.y = rotAngle;
 	}
+
 	if (cdtimer_->GetTime() <= 0) {
-		//XMVECTOR pos = XMLoadFloat3(&transform_.position_);
-		//pos = pos + moveVec[moveDir_];
-		//XMStoreFloat3(&transform_.position_, pos);
 		moveDir_ = MNONE;
 		cdtimer_->StopTimer();
 	}
-	
-
-	//左右キー（A,D)で左右（ｘ座標で）5.0移動する
+	//左右キー（A,D)で左右（ｘ座標で）2.0移動する
 }
 
 void Player::Draw()
